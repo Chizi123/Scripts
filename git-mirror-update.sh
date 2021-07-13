851c5e8ba8e65aafcfd6d03bc4883149424404ce
Joel Grunbaum <joel@jsrv.proxmox.com>
2021-07-14 00:46:41 +1000

    unneeded inversion

--- git-mirror-update.sh
+++ git-mirror-update.sh
@@ -3,7 +3,7 @@
 GIT_DIRECTORY=/Eduardo/git
 
 for d in $GIT_DIRECTORY/*; do
-    if ! grep -q "mirror = true" $d/config; then
-	git -C $d remote update
+    if grep -q "mirror = true" $d/config; then
+	git -C "$d" remote update --prune
     fi
 done
